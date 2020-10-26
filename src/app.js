2176b4ed0c4dad414acc43be5d4d9aef831b79c7
Deusdete Ferreira <deuzinh2010@gmail.com>
2020-10-26 17:27:48 -0300

    Done the functions of obtaining, creating and deleting repository

--- src/app.js
+++ src/app.js
@@ -1,33 +1,66 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const repositoriesTemp = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repositoriesTemp);
+
+  return response.json({...repositoriesTemp});
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const elementIndex = repositories.findIndex((item) => item.id === id);
+
+  if(elementIndex === -1){
+    return response.status(404).json({message: "Repository not found",elementIndex});
+  }
+
+  let repositoriesValue = repositories;
+
+
+  return response.json({repositoriesValue, elementIndex})
+
+  
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoriesTemp = repositories.filter((item) => item.id !== id)
+
+  repositories = repositoriesTemp;
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
 });
 
 module.exports = app;
